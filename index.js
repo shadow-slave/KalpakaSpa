fc1725cbb2d4515ae045274e84c43f790375d589
shadow-slave <ashwinsunil02@gmail.com>
2024-10-19 20:47:31 +0530

    home responsivenes over and whatsapp icon added

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.querySelector('.overlay');
+const menuContent = document.querySelector('.menu-content');
+const social = document.querySelector('.social');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('menu-active'); // Toggle hamburger animation
+    
     if (navLinks.style.height === '100%') {
         navLinks.style.height = '0';  // Close the menu
+        
+        // Remove animation class when menu is closed
+        menuContent.classList.remove('animate'); 
+        social.classList.remove('animate'); 
+
     } else {
         navLinks.style.height = '100%'; // Open the menu
+        
+        // Add animation class when menu is opened
+        menuContent.classList.add('animate'); 
+        social.classList.add('animate'); 
     }
 });
