c975f66aca8e7bd4bdd8f3d7254bbbe64370061e
shadow-slave <ashwinsunil02@gmail.com>
2024-10-21 00:32:41 +0530

    changed images of massages in relaxtherapy.html And updated MENU icon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const hamburger = document.getElementById('hamburger');
 const navLinks = document.querySelector('.overlay');
 const menuContent = document.querySelector('.menu-content');
 const social = document.querySelector('.social');
-
+const menuText = document.querySelector('.menu-text');
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('menu-active'); // Toggle hamburger animation
     
@@ -12,6 +12,7 @@ hamburger.addEventListener('click', () => {
         // Remove animation class when menu is closed
         menuContent.classList.remove('animate'); 
         social.classList.remove('animate'); 
+        menuText.style.display = 'inline';  // Show the previous element
 
     } else {
         navLinks.style.height = '100%'; // Open the menu
@@ -19,5 +20,6 @@ hamburger.addEventListener('click', () => {
         // Add animation class when menu is opened
         menuContent.classList.add('animate'); 
         social.classList.add('animate'); 
+        menuText.style.display = 'none';
     }
 });
